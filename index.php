3cb96258c93eb7487ca352d755b936b184f2a01d
Alexander Blom <blal16@nti.umea>
2018-09-19 10:04:31 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Hemlig sida</title>
+    <link href="https://fonts.googleapis.com/css?family=Monoton" rel="stylesheet">
+    <style>
+      html{
+        background-image: url(BenneBakgrund.png);
+      }
+      main{
+        width: 80%;
+        margin: 0px auto;
+        font-family: 'Monoton', cursive;
+      }
+      fieldset{
+        border: 0px;
+      }
+    </style>
+  </head>
+  <body>
+  <main>
+    <h1>Välkommen</h1>
+    <form action="login.php" method="post">
+      <fieldset>
+          <legend>Logga in</legend>
+          <p>
+              <label for="username">Användarnamn:</label>
+              <input type="text" name="username" id="username">
+          </p>
+          <p>
+              <label for="password">Lösenord:</label>
+              <input type="password" name="password" id="password">
+          </p>
+          <p>
+          <input type="submit" name="submit" value="Logga in" id="submit">
+          </p>
+      </fieldset>
+    </form>
+  </main>
+  </body>
+</html>
